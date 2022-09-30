aec0f6e621aec3021ca30c62169e601500fc77d9
Will Johnson <66628673+willj4945@users.noreply.github.com>
2022-09-30 14:37:02 -0400

    Working on intro

--- Endless Dungeon/Program.cs
+++ Endless Dungeon/Program.cs
@@ -3,6 +3,7 @@
     class Program
     {
         public static Player CurrentPlayer = new Player();
+
         static void Main(String[] args)
         {
             Start();
@@ -13,6 +14,12 @@
             Console.WriteLine("Welcome to Endless Dungeon");
             Console.WriteLine("Enter player name: ");
             CurrentPlayer.Name = Console.ReadLine();
+            Console.WriteLine(
+                $"You are {CurrentPlayer.Name}, a knight living in the kingdom of Larion. You have a steel longsword and" +
+                $" a wooden shield. You are on a quest to defeat the evil dragon of Larion. You've heard he lives up at " +
+                $"the north of the kingdom. You set on the path to defeat him and walk into a dark forest. As you enter " +
+                $"the forest you see that it is getting lighter outside. Day is breaking. It's been a long time since " +
+                $"you've slept. You could rest for a few minutes, and then continue on your path...");
         }
     }
 }
